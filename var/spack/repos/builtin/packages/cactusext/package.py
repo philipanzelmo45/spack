7912f56483d12b5dc7e4014a83a64322e18f6ec3
Erik Schnetter <schnetter@gmail.com>
2016-05-03 21:08:01 -0400

    Finish CactusExt

--- var/spack/repos/builtin/packages/cactusext/package.py
+++ var/spack/repos/builtin/packages/cactusext/package.py
@@ -1,4 +1,5 @@
 from spack import *
+import sys
 
 class Cactusext(Package):
     """Cactus is an open source problem solving environment designed for
@@ -12,27 +13,60 @@ class Cactusext(Package):
     homepage = "http://www.cactuscode.org"
     url      = "https://bitbucket.org/cactuscode/coredoc.git"
 
-    version('master', git='00b516f4704d4a7cb50a1d97e6e8e15b', branch='master')
+    version('master',
+            git='https://bitbucket.org/cactuscode/coredoc.git', branch='master')
 
     variant('funhpc', default=False, description='Enable FunHPC')
+    variant('simulationio', default=False, description='Enable SimulationIO')
 
+    # Actual dependencies
     depends_on("blas")
-    depends_on("fftw")
+    depends_on("fftw +mpi")
     depends_on("gsl")
-    depends_on("hdf5")
+    depends_on("hdf5 +mpi")
     depends_on("hwloc")
     depends_on("lapack")
     depends_on("lua")
     depends_on("mpi")
     depends_on("openssl")
     depends_on("papi")
-    depends_on("petsc")
+    depends_on("petsc +mpi")
     depends_on("zlib")
 
     depends_on("funhpc", when='+funhpc')
+    depends_on("simulationio", when='+simulationio')
+
+    # Configure dependencies for convenience
+    cactusext_compiler = 'gcc@5.3.0-spack'
+    jemalloc_compiler = cactusext_compiler
+    if sys.platform == 'darwin':
+        jemalloc_compiler = 'clang@7.3.0-apple'
+    python_compiler = cactusext_compiler
+    if sys.platform == 'darwin':
+        python_compiler = 'clang@7.3.0-apple'
+
+    depends_on("blas ^openblas")
+    depends_on("boost +mpi")
+    depends_on("hdf5 @1.10.0 +mpi")
+    depends_on("jemalloc %"+jemalloc_compiler)
+    depends_on("lapack ^openblas")
+    depends_on("mpi ^openmpi")
+    openmpi_opts = []
+    try:
+        ibv_devinfo = which('ibv_devinfo')
+        ibv_devinfo()
+        openmpi_opts.append("+verbs")
+    except:
+        pass
+    depends_on("openmpi"+''.join([" "+opt for opt in openmpi_opts]))
+    depends_on("petsc +boost +hdf5 +mpi ~mumps")
+    python_deps = ['bzip2', 'ncurses', 'readline', 'openssl', 'sqlite', 'zlib']
+    depends_on("python %"+python_compiler+
+               ''.join([" ^"+dep+"%"+cactusext_compiler
+                        for dep in python_deps]))
 
     def install(self, spec, prefix):
         # This package does not install anything per se; it only
         # installs many of the dependencies that Cactus-based
         # applications require.
-        mkdir(prefix.lib)
+        mkdirp(prefix.lib)
