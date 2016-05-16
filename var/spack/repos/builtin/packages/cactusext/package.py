a67ca10b82a19fdbd18f46ac22417094d99c6e4b
Erik Schnetter <schnetter@gmail.com>
2016-05-16 19:37:53 -0400

    Combine dependencies

--- var/spack/repos/builtin/packages/cactusext/package.py
+++ var/spack/repos/builtin/packages/cactusext/package.py
@@ -14,109 +14,145 @@ class Cactusext(Package):
     homepage = "http://www.cactuscode.org"
     url      = "https://bitbucket.org/cactuscode/coredoc.git"
 
-    version('master',
-            git='https://bitbucket.org/cactuscode/coredoc.git', branch='master')
+    version("master",
+            git="https://bitbucket.org/cactuscode/coredoc.git", branch="master")
 
-    variant('funhpc', default=False, description='Enable FunHPC')
-    variant('llvm', default=False, description='Enable LLVM')
-    variant('simulationio', default=False, description='Enable SimulationIO')
+    variant("funhpc", default=False, description="Enable FunHPC")
+    variant("julia", default=False, description="Enable Julia")
+    variant("llvm", default=False, description="Enable LLVM")
+    variant("simulationio", default=False, description="Enable SimulationIO")
+
+    deps = {}
+    whens = {}
 
     # Actual dependencies
-    depends_on("blas")
-    depends_on("fftw +mpi +openmp")
-    depends_on("gsl")
-    depends_on("hdf5 +mpi")
-    depends_on("hwloc")
-    depends_on("lapack")
-    depends_on("lua")
-    depends_on("mpi")
-    depends_on("openssl")
-    depends_on("papi")
-    depends_on("petsc +mpi")
-    depends_on("zlib")
+    deps["blas"] = []
+    deps["boost"] = ["+mpi"]
+    deps["fftw"] = ["+mpi", "+openmp"]
+    deps["gsl"] = []
+    deps["hdf5"] = ["+mpi"]
+    deps["hwloc"] = []
+    deps["lapack"] = []
+    deps["lua"] = []
+    deps["mpi"] = []
+    deps["openssl"] = []
+    deps["papi"] = []
+    deps["petsc"] = ["+boost", "+hdf5", "+mpi"]
+    deps["zlib"] = []
     # TODO: Add CUDA
 
-    depends_on("funhpc", when='+funhpc')
-    depends_on("llvm", when='+llvm')
-    depends_on("simulationio", when='+simulationio')
+    whens["funhpc"] = ["+funhpc"]
+    whens["julia"] = ["+julia"]
+    whens["llvm"] = ["+llvm"]
+    whens["simulationio"] = ["+simulationio"]
 
     # Configure dependencies for convenience
 
     # Virtual packages
-    # depends_on("blas ^openblas")
-    # depends_on("lapack ^openblas")
-    # depends_on("mpi ^openmpi")
-    depends_on("openblas")
-    depends_on("openmpi")
+    deps["blas"].append("^openblas")
+    deps["lapack"].append("^openblas")
+    deps["mpi"].append("^openmpi")
+    deps["openblas"] = []
+    deps["openmpi"] = []
+
+    # Initialize dependencies that are mentioned below
+    deps["funhpc"] = []
+    deps["git"] = []
+    deps["jemalloc"] = []
+    deps["julia"] = []
+    # deps["libsigsegv"] = []
+    deps["llvm"] = []
+    deps["python"] = []
+    deps["simulationio"] = []
+
+    whens["jemalloc"] = ["+funhpc"]
 
     # Versions
     # TODO: Remove this once the latest HDF5 version is again the default
-    depends_on("hdf5 @1.10.0")
+    deps["hdf5"].append("@1.10.0")
     # TODO: Remove this once Spack chooses the latest 2.7 version by default
-    depends_on("python @2.7.11")
+    deps["python"].append("@2.7.11")
 
     # Compilers
-    cactusext_compiler = 'gcc@6.1.0-spack'
+    cactusext_compiler = "gcc@6.1.0-spack"
     git_compiler = cactusext_compiler
-    if sys.platform == 'darwin':
-        git_compiler = 'clang@7.3.0-apple'
+    if sys.platform == "darwin":
+        git_compiler = "clang@7.3.0-apple"
     jemalloc_compiler = cactusext_compiler
-    if sys.platform == 'darwin':
-        jemalloc_compiler = 'clang@7.3.0-apple'
+    if sys.platform == "darwin":
+        jemalloc_compiler = "clang@7.3.0-apple"
     python_compiler = cactusext_compiler
-    if sys.platform == 'darwin':
-        python_compiler = 'clang@7.3.0-apple'
+    if sys.platform == "darwin":
+        python_compiler = "clang@7.3.0-apple"
 
-    depends_on("blas %"+cactusext_compiler)
-    depends_on("fftw %"+cactusext_compiler)
-    depends_on("gsl %"+cactusext_compiler)
-    depends_on("hdf5 %"+cactusext_compiler)
-    depends_on("hwloc %"+cactusext_compiler)
-    depends_on("lapack %"+cactusext_compiler)
-    depends_on("lua %"+cactusext_compiler)
-    depends_on("mpi %"+cactusext_compiler)
-    depends_on("openssl %"+cactusext_compiler)
-    depends_on("papi %"+cactusext_compiler)
-    depends_on("petsc %"+cactusext_compiler)
-    depends_on("zlib %"+cactusext_compiler)
+    deps["fftw"].append("%"+cactusext_compiler)
+    deps["gsl"].append("%"+cactusext_compiler)
+    deps["hdf5"].append("%"+cactusext_compiler)
+    deps["hwloc"].append("%"+cactusext_compiler)
+    deps["lua"].append("%"+cactusext_compiler)
+    deps["openssl"].append("%"+cactusext_compiler)
+    deps["papi"].append("%"+cactusext_compiler)
+    deps["petsc"].append("%"+cactusext_compiler)
+    deps["zlib"].append("%"+cactusext_compiler)
 
-    depends_on("funhpc %"+cactusext_compiler, when='+funhpc')
-    depends_on("llvm %"+cactusext_compiler, when='+llvm')
-    depends_on("simulationio %"+cactusext_compiler, when='+simulationio')
+    deps["openblas"].append("%"+cactusext_compiler)
+    deps["openmpi"].append("%"+cactusext_compiler)
+
+    deps["funhpc"].append("%"+cactusext_compiler)
+    deps["julia"].append("%"+cactusext_compiler)
+    deps["llvm"].append("%"+cactusext_compiler)
+    deps["simulationio"].append("%"+cactusext_compiler)
 
     # These are apparently not deduced -- why?
-    depends_on("libsigsegv %"+cactusext_compiler)
-    depends_on("openmpi %"+cactusext_compiler)
+    # deps["libsigsegv"].append("%"+cactusext_compiler)
 
-    git_deps = ['autoconf', 'curl', 'expat', 'openssl', 'zlib']
-    depends_on("git %"+git_compiler+
-               ''.join([" ^"+dep+"%"+cactusext_compiler for dep in git_deps]))
-    depends_on("jemalloc %"+jemalloc_compiler, when='+funhpc')
-    python_deps = ['bzip2', 'ncurses', 'readline', 'openssl', 'sqlite', 'zlib']
-    depends_on("python %"+python_compiler+
-               ''.join([" ^"+dep+"%"+cactusext_compiler
-                        for dep in python_deps]))
+    deps["git"].append("%"+git_compiler)
+    git_deps = ["autoconf", "curl", "expat", "openssl", "zlib"]
+    deps["git"].extend(["^"+dep+" %"+cactusext_compiler for dep in git_deps])
+    deps["jemalloc"].append("%"+jemalloc_compiler)
+    deps["python"].append("%"+python_compiler)
+    python_deps = ["bzip2", "ncurses", "readline", "openssl", "sqlite", "zlib"]
+    deps["python"].extend(
+        ["^"+dep+" %"+cactusext_compiler for dep in python_deps])
 
     # Options
-    depends_on("boost +mpi")
     # TODO: avoid ~polly ~gold
     # ~lldb ~internal_unwind ~polly ~libcxx ~compiler-rt ~gold
-    depends_on("llvm", when='+llvm')
+    # deps["llvm", when="+llvm")
     openmpi_opts = []
     if (os.path.isfile("/usr/include/pmi.h") or
         os.path.isfile("/usr/slurm/include/pmi.h") or
         os.path.isfile("/usr/include/pmi2.h") or
         os.path.isfile("/usr/slurm/include/pmi2.h")):
-        openmpi_opts.append("+pmi")
+        deps["openmpi"].append("+pmi")
     try:
-        ibv_devices = which('ibv_devices')
+        ibv_devices = which("ibv_devices")
         ibv_devices(output=str, error=str)
-        openmpi_opts.append("+verbs")
+        deps["openmpi"].append("+verbs")
     except:
         pass
-    depends_on("openmpi"+''.join([" "+opt for opt in openmpi_opts]))
     # TODO: Mumps doesn't build everywhere; enable it once it works
-    depends_on("petsc +boost +hdf5 +mpi ~mumps")
+    deps["petsc"].append("~mumps")
+
+    # Set dependencies
+    for pkg, opts in sorted(deps.iteritems()):
+        try:
+            when = " ".join(whens[pkg])
+        except:
+            when = None
+        print pkg + " " + " ".join(opts), ("when=" + when if when else "")
+        depends_on(pkg + " " + " ".join(opts), when=when)
+
+    # depends_on("cereal %gcc@6.1.0-spack")
+    # depends_on("cmake %gcc@6.1.0-spack")
+    # depends_on("curl %gcc@6.1.0-spack")
+    # depends_on("hwloc %gcc@6.1.0-spack")
+    # depends_on("libpciaccess %gcc@6.1.0-spack")
+    # depends_on("libtool %gcc@6.1.0-spack")
+    # depends_on("m4 %gcc@6.1.0-spack")
+    # depends_on("ncurses %gcc@6.1.0-spack")
+    # depends_on("qthreads %gcc@6.1.0-spack")
+    # depends_on("swig %gcc@6.1.0-spack")
 
     def install(self, spec, prefix):
         # This package does not install anything per se; it only
